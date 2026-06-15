e7803f787c1fb7dc7b06ae417236396cc33a14a7
florent.wagner <florent.wagner@cgi.com>
2026-06-15 11:14:29 +0200

    Extract frontend assets

---
+++ public/assets/scripts/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('submit', (event) => {
+    const form = event.target;
+
+    if (!(form instanceof HTMLFormElement)) {
+        return;
+    }
+
+    const message = form.dataset.confirmMessage;
+
+    if (message && !window.confirm(message)) {
+        event.preventDefault();
+    }
+});
